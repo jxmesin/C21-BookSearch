729aa20e643c7a1d3db15ea6b30cb609ee32d9b3
james <arvinjamesinanoria@gmail.com>
2023-03-14 18:43:55 -0700

    readme file

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -4,17 +4,30 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// Import the Apollo client.
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client"
+
+// Create the Apollo client.
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+})
+
 function App() {
   return (
     <Router>
-      <>
+      <ApolloProvider>
         <Navbar />
         <Switch>
           <Route exact path='/' component={SearchBooks} />
           <Route exact path='/saved' component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
-      </>
+      </ApolloProvider>
     </Router>
   );
 }
